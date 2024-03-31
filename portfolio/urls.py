479817fb11ca8dbd768f1ed430e544d569628a83
Jean eudes adamah Lossou <adamahjean@gmail.com>
2024-03-31 03:20:31 +0000

    first commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('portfolio-details', portfolio_details, name='portfolio_details'),
+    path('portfolio-details', portfolio_details, name='portfolio-details'),
     path('inner-page', inner_page, name='inner-page'),
 ]
